08653d706a910bedd81fd0b40806b1f6086e9121
Kavindu Madushan <e209563@esoft.academy>
2026-07-18 06:34:38 +0530

    Fix PDF worker MIME type on Nginx

--- src/utils/outForDeliveryPdf.js
+++ src/utils/outForDeliveryPdf.js
@@ -1,5 +1,5 @@
 import { getDocument, GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf.mjs";
-import pdfWorkerUrl from "pdfjs-dist/legacy/build/pdf.worker.min.mjs?url";
+import pdfWorkerUrl from "pdfjs-dist/legacy/build/pdf.worker.min.mjs?worker&url";
 import { findRiderName, findTrackingColumn } from "./outForDeliveryText.js";
 
 GlobalWorkerOptions.workerSrc = pdfWorkerUrl;
